ac4ba119862290c439f182444205fb18e572cbb6
Nesbert Hidalgo <nesbert@gmail.com>
2012-10-03 08:44:55 -0700

    Object updates..
    - Added support for ArrayObject values
    - Class is no longer abstract.. base functionality is now exposed
    - Unit tests

--- Object.php
+++ Object.php
@@ -6,7 +6,7 @@ namespace NObjects;
  *
  * @author Nesbert Hidalgo
  */
-abstract class Object
+class Object
 {
     // instance methods
 
@@ -49,6 +49,8 @@ abstract class Object
             // if set method exists for a key set value
             if (method_exists($this, $method)) {
                 $this->{$method}($v);
+            } else {
+                $this->{$k} = $v;
             }
         }
         return $this;
@@ -69,23 +71,48 @@ abstract class Object
             $props[$class] = $this->__getAllReflectionProperties(new \ReflectionClass($this));
         }
 
+        // adding support for public properties
+        if (empty($props[$class])) {
+            $props[$class] = array();
+            foreach ($this as $k => $v) {
+                $props[$class][] = (object)array(
+                    'name' => $k,
+                    'value' => $v,
+                );
+            }
+        }
+
         $array = array();
         foreach ($props[$class] as $rp) {
+
             $func = "get{$rp->name}";
 
             // skip properties that start with an _
             // skip functions that don't exist
             if ($rp->name{0} == '_'
-                || !method_exists($this, $func)) {
+                || (!method_exists($this, $func) && !isset($this->{$rp->name}))
+                ) {
                 continue;
             }
-            $val = $this->$func();
+
+            // if public use temp object and don't cache
+            if (isset($this->{$rp->name})) {
+                $val = $rp->value;
+                unset($props[$class]);
+            } else {
+                $val = $this->$func();
+            }
 
             $base = __CLASS__;
 
             if ($val instanceof $base) {
                 $array[$rp->name] = $val->toArray();
-            } else if (is_array($val)) {
+            } else if (is_array($val) || ($val instanceof \ArrayObject)) {
+
+                if ($val instanceof \ArrayObject) {
+                    $val = $val->getArrayCopy();
+                }
+
                 foreach ($val as $k => $v) {
                     if ($v instanceof $base) {
                         $val[$k] = $v->toArray();
---
+++ tests/ObjectTest.php
@@ -0,0 +1,73 @@
+<?php
+namespace NObjects\Tests;
+
+use NObjects\Object;
+
+class ObjectTest extends \PHPUnit_Framework_TestCase
+{
+    public function testObjectHelpers()
+    {
+        // simple
+        $data = array('test' => 1212);
+        $obj = new \NObjects\Object();
+        $this->assertTrue($obj instanceof Object);
+        $this->assertEquals($obj, $obj->fromArray($data));
+        $this->assertTrue(isset($obj->test));
+        $this->assertEquals($data['test'], $obj->test);
+        $this->assertEquals($data, $obj->toArray());
+        $this->assertEquals(json_encode($data), $obj->toJSON());
+        $this->assertEquals(array_keys($data), $obj->getProperties());
+        $this->assertEquals(array_values($data), $obj->getPropertyValues());
+
+        // simple 2 load via construct
+        $obj2 = new \NObjects\Object($data);
+        $this->assertTrue($obj2 instanceof Object);
+        $this->assertTrue(isset($obj2->test));
+        $this->assertEquals($data['test'], $obj2->test);
+        $this->assertEquals($data, $obj2->toArray());
+        $this->assertEquals(json_encode($data), $obj2->toJSON());
+        $this->assertEquals(array_keys($data), $obj2->getProperties());
+        $this->assertEquals(array_values($data), $obj2->getPropertyValues());
+
+        // complex
+        $data3 = array(
+            'id' => 555,
+            'foo' => 'bar',
+            'roles' => array('Father','Husband','Geek','Developer','Thinker'),
+            'movie' => (object) array('name' => 'Star Wars'),
+            'favorites' => new \NObjects\Object( (object)array('toy'=>'Legos') ),
+        );
+
+        $data3['nested'] = $data3;
+
+        $obj3 = new \NObjects\Object($data3);
+
+
+        $this->assertTrue($obj3 instanceof Object);
+
+        foreach ($data3 as $k => $v) {
+            $this->assertTrue(isset($obj3->{$k}));
+            $this->assertEquals($data3[$k], $obj3->{$k});
+        }
+
+        $this->assertTrue(is_array($obj3->toArray()));
+        $this->assertEquals(json_encode($data3), $obj3->toJSON());
+        $this->assertNotEquals(json_encode($data), $obj3->toJSON());
+        $this->assertEquals(array_keys($data3), $obj3->getProperties());
+
+        // NObject get translated to an array
+        $this->assertNotEquals(array_values($data3), $obj3->getPropertyValues());
+    }
+
+    public function testAncestors()
+    {
+        $ancestors = Object::ancestors('Object');
+        $this->assertTrue(empty($ancestors));
+
+        $ancestors = Object::ancestors('\NObjects\DateTime');
+        $this->assertFalse(empty($ancestors));
+        $this->assertTrue(is_array($ancestors));
+        $this->assertTrue(count($ancestors)==1);
+        $this->assertEquals('DateTime', $ancestors[0]);
+    }
+}
