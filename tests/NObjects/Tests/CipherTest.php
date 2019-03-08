fac518169235f1c33ded133cdfb3e3487c7526f4
Scott Moorhouse <Scott.Moorhouse@allegiantair.com>
2019-03-08 13:27:25 -0800

    Reverted to mcrypt rijndael because of differences with openssl aes-256 when key size is not 32 bytes. Deprecate all Cipher levels.

--- tests/NObjects/Tests/CipherTest.php
+++ tests/NObjects/Tests/CipherTest.php
@@ -9,11 +9,6 @@ use NObjects\Cipher;
  */
 class CipherTest extends \PHPUnit_Framework_TestCase
 {
-    /**
-     * @var Cipher
-     */
-    protected $o;
-
     protected function setUp()
     {
         if (\PHP_MAJOR_VERSION > 5 || (\PHP_MAJOR_VERSION == 5 && \PHP_MINOR_VERSION > 5)) {
@@ -21,39 +16,28 @@ class CipherTest extends \PHPUnit_Framework_TestCase
               'MCrypt functionality throws deprecation errors after PHP 5.5'
             );
         }
-
-        $this->o = new Cipher;
     }
 
-    protected function tearDown()
-    {
-        unset($this->o);
-    }
-
-    protected function all($s, $l = 1, $k = null)
-    {
-        $e = $this->o->encrypt($s, $l, $k);
-        $this->assertEquals($e, Cipher::encrypt($s, $l, $k));
-
-        $d = $this->o->decrypt($e, $l, $k);
-        $this->assertEquals($s, $d);
-        $this->assertEquals($d, Cipher::decrypt($e, $l, $k));
-        $this->assertEquals($s, Cipher::decrypt($e, $l, $k));
-
-        if ($k) {
-            $this->assertNotEquals($d, Cipher::decrypt($e, $l, $k.'m'));
-            $this->assertNotEquals($s, Cipher::decrypt($e, $l, $k.'m'));
-        }
-    }
+//    protected function all($s, $l = 1, $k = null)
+//    {
+//        $this->assertEquals($e, Cipher::encrypt($s, $l, $k));
+//
+//        $this->assertEquals($s, $d);
+//        $this->assertEquals($d, Cipher::decrypt($e, $l, $k));
+//        $this->assertEquals($s, Cipher::decrypt($e, $l, $k));
+//
+//        if ($k) {
+//            $this->assertNotEquals($d, Cipher::decrypt($e, $l, $k.'m'));
+//            $this->assertNotEquals($s, Cipher::decrypt($e, $l, $k.'m'));
+//        }
+//    }
 
     /**
      * @group legacy
      */
     public function testEncrypt()
     {
-        $s = 'www.creovel.org';
-        $e = $this->o->encrypt($s);
-        $this->assertEquals($e, Cipher::encrypt($s));
+        $this->assertEquals('cgS4/O5hZXWxLNNo77UhmIZ5vcRmA04svVftYNdVFeU=', Cipher::encrypt('www.creovel.org', 4, '12345678901234567890123456789012'));
     }
 
     /**
@@ -61,27 +45,9 @@ class CipherTest extends \PHPUnit_Framework_TestCase
      */
     public function testDecrypt()
     {
-        $s = 'www.creovel.org';
-        $this->all($s);
-
-        // level tests
-        $this->all($s, 2);
-        $this->all($s, 3);
-        $this->all($s, 5); // will default to 1
-
-        // level tests with key/salt
-        $k = 'keepitDRY';
-        $this->all($s, 1, $k);
-        $this->all($s, 2, $k);
-        $this->all($s, 3, $k);
+        $this->assertEquals('www.creovel.org', Cipher::decrypt('cgS4/O5hZXWxLNNo77UhmIZ5vcRmA04svVftYNdVFeU=', 4, '12345678901234567890123456789012'));
     }
 
-    public function testDecryptRijndael()
-    {
-        $s = 'www.creovel.org';
-        $this->all($s, 4);
-        $this->all($s, 4, 'keepitDRY');
-    }
 
     /**
      * @group legacy
